bba7805b406057c83ca812b96265412c8b7e507d
Lance Lan Meng <36537676+freeelancer@users.noreply.github.com>
2025-12-29 15:10:28 +0800

    fix: block evm tx before provider channel creation (#39)

--- app/ante/evm.go
+++ app/ante/evm.go
@@ -4,12 +4,15 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	chainante "github.com/cosmos/evm/ante"
 	evmante "github.com/cosmos/evm/ante/evm"
+	consumerante "github.com/cosmos/interchain-security/v7/app/consumer/ante"
 )
 
 func newEVMAnteHandler(ctx sdk.Context, options HandlerOptions) sdk.AnteHandler {
+	consumerMsgFilter := consumerante.NewMsgFilterDecorator(options.ConsumerKeeper)
 	evmParams := options.EvmOptions.EvmKeeper.GetParams(ctx)
 	feemarketParams := options.EvmOptions.FeeMarketKeeper.GetParams(ctx)
 	decorators := []sdk.AnteDecorator{
+		consumerMsgFilter,
 		evmante.NewEVMMonoDecorator(
 			options.EvmOptions.AccountKeeper,
 			options.EvmOptions.FeeMarketKeeper,
